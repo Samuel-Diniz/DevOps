6302250f855950331045e5f9cc07f17cfa2b41c5
root <root@Samuel-Diniz-Finx.>
2024-11-25 13:56:55 -0300

    Mensagem explicando as mudanças

--- app/app.py
+++ app/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Bem-vindo à Salaryfits! Desafio DevOps concluído com sucesso."
 
+@app.route("/health")
+def health():
+    return "OK", 200  # Resposta de sucesso para indicar que a aplicação está funcionando.
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
